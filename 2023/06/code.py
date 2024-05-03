9f6f53b6bcc3ea538e002058b8b0c552db456df0
Eir Wilson <git@eirrw.com>
2024-05-03 11:55:56 -0400

    day 6 part 2

--- 2023/06/code.py
+++ 2023/06/code.py
@@ -1,23 +1,18 @@
 with open('input') as fp:
-    times = [int(i) for i in fp.readline().strip().split()[1:]]
-    recs = [int(i) for i in fp.readline().strip().split()[1:]]
+    time = int(''.join(fp.readline().strip().split()[1:]))
+    rec = int(''.join(fp.readline().strip().split()[1:]))
 
-prod = 1
-for i in range(len(times)):
-    print('Race {}: time {}, record {}'.format(i+1, times[i], recs[i]))
-    wins = 0
-    for x in range(times[i] + 1):
-        time_rem = times[i] - x
-        dist = x * time_rem
+print('Race: time {}, record {}'.format(time, rec))
+wins = 0
+for x in range(time + 1):
+    time_rem = time - x
+    dist = x * time_rem
 
-        win = False
-        if dist > recs[i]:
-            wins += 1
-            win = True
+    win = False
+    if dist > rec:
+        wins += 1
+        win = True
 
-        print('hold: {}, dist: {}, win: {}'.format(x, dist, win))
+    #print('hold: {}, dist: {}, win: {}'.format(x, dist, win))
 
-    print('wins: {}'.format(wins))
-    prod *= wins
-
-print('Total: {}'.format(prod))
+print('wins: {}'.format(wins))
