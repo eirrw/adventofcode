678877dc6a596573d7f3c2e0ab66a06ad8d5d62f
Eir Wilson <git@eirrw.com>
2024-05-04 11:28:50 -0400

    day 9 part 2

--- 2023/09/code.py
+++ 2023/09/code.py
@@ -1,7 +1,4 @@
-all_data = []
-
-
-def get_diff(data):
+def get_diff(data, history=False):
     diffs = []
     for i in range(len(data) - 1):
         diff = data[i + 1] - data[i]
@@ -11,18 +8,26 @@ def get_diff(data):
     if len(check) == 1 and 0 in check:
         return 0
 
-    new_diff = get_diff(diffs)
-    return diffs[-1] + new_diff
+    new_diff = get_diff(diffs, history)
+    if not history:
+        return diffs[-1] + new_diff
+    else:
+        return diffs[0] - new_diff
 
 
+all_data = []
 with open('input') as fp:
     for line in fp:
         all_data.append([int(i) for i in line.strip().split()])
 
 print(all_data)
 data_sum = 0
+prev_data_sum = 0
 for row in all_data:
     next_val = row[-1] + get_diff(row)
+    prev_val = row[0] - get_diff(row, True)
     data_sum += next_val
+    prev_data_sum += prev_val
 
 print(data_sum)
+print(prev_data_sum)
