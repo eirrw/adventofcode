d8af5322d014269e5d74779fecee0fea468bf73e
Eir Wilson <git@eirrw.com>
2024-05-05 13:41:19 -0400

    day 11 part 2
    this is so efficient (time wise) it's almost stupid

--- 2023/11/code.py
+++ 2023/11/code.py
@@ -1,45 +1,52 @@
 image = []
+scale = 1000000
 
 with open('input') as fp:
     for line in fp:
         row = list(line.strip())
-
-        # expand rows as we add them
-        if len(set(row)) == 1 and '.' in row:
-            image.append(list(line.strip()))
         image.append(list(line.strip()))
 
-mirror_image = []
-for x in range(len(image[0])):
-    row = [image[i][x] for i in range(len(image))]
-    if len(set(row)) == 1 and '.' in row:
-        mirror_image.append(row)
-    mirror_image.append(row)
-
-
+rows = []
 for row in image:
-    print(' '.join(row))
-print()
-for row in mirror_image:
-    print(' '.join(row))
+    if len(set(row)) == 1:
+        rows.append(scale)
+    else:
+        rows.append(1)
+
+cols = []
+for x in range(len(image[0])):
+    col = [image[i][x] for i in range(len(image))]
+    if len(set(col)) == 1:
+        cols.append(scale)
+    else:
+        cols.append(1)
+
+# for row in image:
+#     print(' '.join(row))
+# print(rows)
+# print(cols)
 
 galaxy_locs = []
-for y in range(len(mirror_image)):
-    for x in range(len(mirror_image[y])):
-        if mirror_image[y][x] == '#':
-            galaxy_locs.append((x, y))
+for y in range(len(image)):
+    galaxy_locs.extend([(x, y) for x, g in enumerate(image[y]) if g == '#'])
 
 print(galaxy_locs)
-
 
 dist = 0
 for i in range(len(galaxy_locs)):
     galaxy = galaxy_locs[i]
 
     for other in galaxy_locs[i+1:]:
-        dist_x = abs(galaxy[0] - other[0])
-        dist_y = abs(galaxy[1] - other[1])
-        print("{} -> {}: x{}, y{}, t{}".format(galaxy, other, dist_x, dist_y, dist_x+dist_y))
+        min_x = min(galaxy[0], other[0])
+        max_x = max(galaxy[0], other[0])
+        min_y = min(galaxy[1], other[1])
+        max_y = max(galaxy[1], other[1])
+
+        dist_x = sum(cols[min_x+1:max_x+1])
+
+        dist_y = sum(rows[min_y+1:max_y+1])
+
+#        print("{} -> {}: x{}, y{}, t{}".format(galaxy, other, dist_x, dist_y, dist_x+dist_y))
 
         dist += dist_x + dist_y
 
