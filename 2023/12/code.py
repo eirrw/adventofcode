c44a1ab8a7248b726b89390782b70ae1eebe22b5
Eir Wilson <git@eirrw.com>
2024-05-07 11:38:25 -0400

    day 12 part 2 v2
    dynamic programming is a new concept to me. spent a few days studying
    the help qs and solutions on the AoC reddit. this final solution brought
    to you by the comment at
    https://old.reddit.com/r/adventofcode/comments/18ghux0/2023_day_12_no_idea_how_to_start_with_this_puzzle/kd0npmi/
    and the walkthrough at
    https://advent-of-code.xavd.id/writeups/2023/day/12/ .

--- 2023/12/code.py
+++ 2023/12/code.py
@@ -1,37 +1,43 @@
 from collections import namedtuple
-from itertools import product
-import re
+from functools import cache
 
 Record = namedtuple('Record', ['data', 'counts'])
+scale = 5
 
 records = []
-with open('example1') as fp:
+with open('input') as fp:
     for line in fp:
         data = line.strip().split()
         print(data)
-        records.append(Record('?'.join([data[0]]*5), [int(i) for i in data[1].split(',')]*5))
+        records.append(Record('?'.join([data[0]]*scale), [int(i) for i in data[1].split(',')]*scale))
 
 print(records)
 
-count_perms = 0
+
+@cache
+def compute_arrangments(data, counts):
+    if not counts:
+        return 1 if '#' not in data else 0
+    if not data:
+        return 0
+
+    result = 0
+    if data[0] in '.?':
+        result += compute_arrangments(data[1:], counts)
+    if data[0] in '#?':
+        if (
+            counts[0] <= len(data)
+            and '.' not in data[:counts[0]]
+            and (len(data) == counts[0] or data[counts[0]] != '#')
+        ):
+            result += compute_arrangments(data[counts[0]+1:], counts[1:])
+
+    return result
+
+
+total_arrangements = 0
 for record in records:
-    count = record.data.count('?')
-    exist = record.data.count('#')
-    print('Record: {}'.format(record))
-    if count > 0:
-        regex = r'^\.*' + r'\.+'.join([f"#{{{x}}}" for x in record.counts]) + r'\.*$'
-        print(regex)
+    print(f"Processing {record}")
+    total_arrangements += compute_arrangments(record.data, tuple(record.counts))
 
-        for rep in product(['.', '#'], repeat=count):
-            data = record.data
-            for x in rep:
-                data = data.replace('?', x, 1)
-            if data.count('#') != sum(record.counts) - exist:
-                continue
-
-            if re.match(regex, data):
-                count_perms += 1
-
-            #print(data)
-
-print(count_perms)
+print(total_arrangements)
