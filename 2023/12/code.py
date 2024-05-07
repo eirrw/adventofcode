eaaf5b760231c8c1d1b4c705b8e4792ff2fbb378
Eir Wilson <git@eirrw.com>
2024-05-07 11:21:03 -0400

    day 12 part 2 v1
    my stab at extending my p1 solution to p2. I think in theory this will
    work but it might take years to complete lol.

--- 2023/12/code.py
+++ 2023/12/code.py
@@ -5,26 +5,29 @@ import re
 Record = namedtuple('Record', ['data', 'counts'])
 
 records = []
-with open('input') as fp:
+with open('example1') as fp:
     for line in fp:
         data = line.strip().split()
-        records.append(Record(data[0], [int(i) for i in data[1].split(',')]))
+        print(data)
+        records.append(Record('?'.join([data[0]]*5), [int(i) for i in data[1].split(',')]*5))
 
 print(records)
 
 count_perms = 0
 for record in records:
     count = record.data.count('?')
+    exist = record.data.count('#')
     print('Record: {}'.format(record))
     if count > 0:
-        reps = product(['.', '#'], repeat=count)
         regex = r'^\.*' + r'\.+'.join([f"#{{{x}}}" for x in record.counts]) + r'\.*$'
         print(regex)
 
-        for rep in reps:
+        for rep in product(['.', '#'], repeat=count):
             data = record.data
             for x in rep:
                 data = data.replace('?', x, 1)
+            if data.count('#') != sum(record.counts) - exist:
+                continue
 
             if re.match(regex, data):
                 count_perms += 1
